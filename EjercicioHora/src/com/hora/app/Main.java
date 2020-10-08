48573b41759130c8177609fb378a2169b887f6a7
Sergio Zúñiga Ramos <20201@CALASANZSALAMANCA.local>
2020-10-08 13:01:10 +0200

    Ejercicio Fecha

---
+++ EjercicioHora/src/com/hora/app/Main.java
@@ -0,0 +1,52 @@
+package com.hora.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.GregorianCalendar;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Main() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html"); 
+		
+		PrintWriter outPrintWriter= response.getWriter();
+		
+		Date objFecha = new Date();
+		
+		outPrintWriter.print("<html><body>");
+		outPrintWriter.print("<h1>La fecha es: </h1>"+ objFecha);
+		outPrintWriter.print("</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
